98968b23e609c04300aa14c1e065fde22a3fb3dd
kaniery <133332961+kaniery@users.noreply.github.com>
2024-01-31 14:15:27 +0900

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,29 @@
-import '../App.css'
-import React from 'react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import "../App.css";
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const baseURL = "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads?offset=7";
+const baseURL = "https://railway.bulletinboard.techtrain.dev/threads?offset=0";
 
 export const Home = () => {
-    const [thread, setThread] = useState([]);
-  
-    useEffect(() => {
-      axios.get(baseURL)
-      .then((respons) => {
-        console.log(respons.data)
-        setThread(respons.data)
-      }) 
-    }, [])
+  const [thread, setThread] = useState([]);
 
-    return(
-        <div className='home'>
-            <h1>新着スレッド</h1>
-            <ul>
-            {thread.map(( item, index ) => (
-                <li key={ index }>{ item.title }</li>
-            ))}
-            </ul>
-        </div>
-  )
-}
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      console.log(response.data);
+      setThread(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>新着スレッド</h1>
+      <ul>
+        {thread.map((item, index) => (
+          <li key={index}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default Home;
