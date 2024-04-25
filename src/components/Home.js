2a74e59fe727d7c44a55061193142656c5b17453
kaniery <133332961+kaniery@users.noreply.github.com>
2024-04-25 13:54:29 +0900

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,21 +6,26 @@ import { useEffect, useState } from "react";
 const baseURL = "https://railway.bulletinboard.techtrain.dev/threads?offset=0";
 
 export const Home = () => {
-  const [thread, setThread] = useState([]);
+  const [threads, setThreads] = useState([]);
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      console.log(response.data);
-      setThread(response.data);
-    });
+    axios
+      .get(baseURL)
+      .then((response) => {
+        console.log(response.data);
+        setThreads(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
     <div className="home">
       <h1>新着スレッド</h1>
       <ul>
-        {thread.map((item, index) => (
-          <li key={index}>{item.title}</li>
+        {threads.map((item) => (
+          <li key={item.id}>{item.title}</li>
         ))}
       </ul>
     </div>
---
+++ src/components/Thread.js
@@ -0,0 +1,9 @@
+import React from "react";
+//import axios from "axios";
+import "../App.css";
+
+export const Thread = () => {
+  return <div>スレッドの名前</div>;
+};
+
+export default Thread;
